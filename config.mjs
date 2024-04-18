ccc56799a8fcb2b82b6389574db6a5f7238cc7b7
r4ynex7k <hasansabbahnetwork@gmail.com>
2024-04-18 22:36:49 +0300

    discord-vanity-url-sniper
    50 satir tls bot publamak istediğim icin buraya atiyorum bir işe yaramaz ama geliştirirseniz güzel şeyler olabilir // discord.gg/7k

---
+++ config.mjs
@@ -0,0 +1,5 @@
+export const token = "TOKEN";
+export const channelId = "CHANNEL_ID";
+export const serverId = "SERVER_ID";
+export const gateway = "wss://gateway.discord.gg/?v=9&encoding=json&shard=0&shardcount=1&region=afg-east";
+export const address = "pax.discord.com";
